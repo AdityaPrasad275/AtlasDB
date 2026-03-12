2a233a6c987f852afba609c4702e125db041d12a
AP <adityanprasad275@gmail.com>
2026-03-12 21:30:32 +0530

    deletePage added in buffer pool manager

--- include/BufferPoolManager.h
+++ include/BufferPoolManager.h
@@ -1,9 +1,11 @@
 #include<unordered_map>
 #include <list>
+#include <mutex>
 
 #include "DiskManager.h"
 #include "Page.h"
 #include "type.h"
+#include "LRUReplacer.h"
 
 class BufferPoolManager {
 public:
@@ -15,8 +17,8 @@ public:
     bool unpinPage(page_id_t page_id, bool is_dirty); //"I'm done with Page #5." decerements page_count, if is_dirty, we remerr to flush to disk later
 
     Page* newPage(page_id_t &page_id); //Give me a brand-new, empty page." The BPM allocates a new page_id, finds a slot in RAM, and clears it.
-    // should it be a pointer be here or pass by reference
-
+     
+    bool deletePage(page_id_t page_id); // delete page from file
 
     bool flushPage(page_id_t page_id); // "Force Page #5 to disk right now." (Usually used for WAL/Logging).
 
@@ -31,9 +33,11 @@ private:
     std::unordered_map<page_id_t, frame_id_t> _page_table;
     // which page belongs to which frame
 
-    // LRUReplacer* _replacer; // which page to evict
+    LRUReplacer* _replacer; // which page to evict
     std::list<frame_id_t> _free_frames_list;
 
     // Find whcih frame to use (either from free frames or evict using lru replacer)
     bool findVictim(frame_id_t *frame_id);
+
+    std::mutex _latch;
 };
